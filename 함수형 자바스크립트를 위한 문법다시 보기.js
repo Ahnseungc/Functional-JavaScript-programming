136674816cab42ecbce90f6151a12882f4057126
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2023-07-17 15:20:47 +0900

    07-17

--- 함수형 자바스크립트를 위한 문법다시 보기.js
+++ 함수형 자바스크립트를 위한 문법다시 보기.js
@@ -109,7 +109,7 @@ console.log(10, 5);
 console.log(10, "a");
 //Error
 
-//즉시 실행
+// 익명 함수 즉시 실행
 (function (a) {
   console.log(a);
 })(100);
@@ -121,3 +121,148 @@ function f1() {
 }
 
 f1();
+
+var pj = new (function () {
+  this.name = "PJ";
+  this.age = 28;
+  this.constructor.prototype.hi = function () {
+    console.log("h1");
+  };
+})();
+console.log(pj);
+//hi
+pj.hi();
+//hi
+
+//간단 버전 문자열 화살표 함수와 new Funciton 성능
+function L(str) {
+  var splitted = str.split("=>");
+  return new Function(splitted[0], "return (" + splitted[1] + ")");
+}
+
+console.log(L("n => n*10")(10));
+
+console.time("익명 함수");
+for (var i = 0; i < 10000; i++) {
+  (function (v) {
+    return v;
+  })(i);
+}
+console.timeEnd("익명 함수");
+
+console.time("new Function");
+for (var i = 0; i < 10000; i++) {
+  L("v => v")(i);
+}
+console.timeEnd("new Function");
+
+console.log("1");
+var arr = Array(10000);
+// _map(arr,function(v,i){
+//   return i*2;
+// })
+// console.timeEnd("1");
+//1 : 0.5ms~0.7ms
+
+// console.time("2");
+
+var arr = Array(10000);
+// _map(arr, L('v, i=> i*2'));
+// console.timeEnd("2");
+//2 0.5ms~0.8ms  시간차이가 안나는 이유는 익명함수의 경우 10,000번 반복되었지만, new Function은 한번만 실행 된다
+
+//메모이제이션 기법
+function L2(str) {
+  if (L2[str]) return L2[str]; //혹시 이미 같은 함수가 있다면 즉시 리턴
+  var splitted = str.split("=>");
+  return new Function(splitted[0], "return (" + splitted[1] + ")");
+}
+
+//유명 함수
+var f1 = function () {
+  console.log(f1);
+};
+
+//아주 안전하고 편한 자기 참조
+
+var hi = 1;
+var hello = function hi() {
+  console.log(hi);
+};
+
+hello();
+
+console.log(hi);
+console.log(++hi);
+
+hello();
+
+console.log(hello.name == "hi");
+
+var z1 = function z() {
+  console.log(z, 1);
+};
+
+var z2 = function z() {
+  console.log(z, 2);
+};
+
+z1();
+z2();
+
+console.log(z1.name === z2.name);
+
+//유명 함수를 이용한 재귀
+
+function flatten(arr) {
+  return (function f(arr, new_arr) {
+    arr.forEach(function (v) {
+      Array.isArray(v) ? f(v, new_arr) : new_arr.push(v);
+    });
+    return new_arr;
+  })(arr, []);
+}
+
+flatten([1, [2], [3, 4]]);
+
+// () 다시보기
+function test(a, b, c) {
+  console.log("a b c :", a, b, c);
+  console.log("this:", this);
+  console.log("arguments:", arguments);
+}
+
+test(10);
+test(10, undefined);
+test(10, 20, 30);
+
+function test2(a, b) {
+  b = 10;
+  console.log(arguments);
+}
+
+test2(1);
+//[1]
+
+test2(1, 2);
+//[1,10]
+
+var obj1 = {
+  0: 1,
+  1: 2,
+};
+
+console.log(obj1);
+
+var a = obj1[0];
+var b = obj1[1];
+b = 10;
+
+console.log(obj1);
+// {0: 1, 1: 2} 바뀌지 않음 인자와 변수의 차이
+
+//this 다시 보기
+
+var o1 = { name: "obj1" };
+o1.test = test;
+o1.test(3, 2, 1);
